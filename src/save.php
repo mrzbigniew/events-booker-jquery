b1a8e021866f056714ff8b6a01f39729b5419a88
Zbigniew Jędraczka <mr.zbigniew@gmail.com>
2018-03-14 22:22:54 +0100

    changed save basket functioin to use post method

--- src/save.php
+++ src/save.php
@@ -1,8 +1,2 @@
 <?php
-header('Content-Type: text/json; charset=UTF-8');
-$data = file_get_contents('php://input');
-$objData = json_decode($data);
-
-foreach($objData as $obj) {
-    var_dump($obj);
-}
+var_dump($_POST);
